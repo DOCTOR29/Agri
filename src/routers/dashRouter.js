f11cb7d4d421cc05eb47209ba85617758ee69891
Doctor <naveedislam545@gmail.com>
2023-09-07 18:24:48 +0530

    graph and index

--- src/routers/dashRouter.js
+++ src/routers/dashRouter.js
@@ -1,22 +1,49 @@
 const express = require('express')
 const DFF = require('../models/modelDff')
+const DF = require('../models/modelDF')
+const DI = require('../models/ModelDI')
 const router = express.Router()
 require('../db/conn.js');
 
 
 router.get('/', async (req, res) => {
-     res.render('index', )
+    var countFinancing = 0;
+    var sumLoanAmountFPO = 0;
+    var financingFarmersCount = 0;
+    var sumLoanAmount = 0;
+    var insuranceCount = 0;
+    const formDF = await DF.find()
+    const formDFF = await DFF.find()
+    const formDI = await DI.find()
+
+    formDI.forEach(() => insuranceCount++)
+
+    formDFF.forEach((entry) => {
+        financingFarmersCount++
+        sumLoanAmount += entry.loanAmount
+    })
+    formDF.forEach(() => countFinancing++)
+    formDF.forEach((entry) => {
+        sumLoanAmountFPO += entry.loanAmount
+    })
+
+    res.render('index', {
+        countFinancing, sumLoanAmountFPO,
+        financingFarmersCount,
+        sumLoanAmount, insuranceCount
+    })
 })
 
 router.get('/form/data', async (req, res) => {
     const formDFF = await DFF.find()
-    var countMale =0, countFemale = 0 
+       var countMale =0, countFemale = 0 
     formDFF.forEach((form) => {
         if(form.gender ==='Male')
         { countMale++ }
         else {
             countFemale++
         }
+    
         
        
     });
