83cba7622c4f447fd836c61a985ef52e9bd82873
Doctor <naveedislam545@gmail.com>
2023-10-05 20:16:47 +0530

    data1

--- src/routers/aggregate1Router.js
+++ src/routers/aggregate1Router.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const json2csv = require('json2csv').parse
 
 
 const countName = async function (varr) {
@@ -42,6 +42,116 @@ const sumLoan1 = async function (varr) {
     });
     return num
 }
+
+router.get('/aggregate1/data', async (req, res) => { 
+    var data = {
+        card1: 0,
+        card2: 0,
+        card3: 0,
+        card4: 0,
+        card5: 0,
+        card6: 0,
+        card7: 0,
+        card8: 0,
+        card9: 0,
+        card10: 0,
+        card11: 0,
+        card12: 0,
+    
+        name1:'Total Number of Beneficiaries',
+        name2:'Number of FPOs receiving credit',
+        name3:'Credit to FPOs',
+        name4:'Number of VLEs receiving credit',
+        name5:'Value of VLEs credit',
+        name6:'Number of Individual farmers receiving Credit',
+        name7:'Value of credit to individual farmers',
+        name8: 'Number of savings account opened',
+        name9: 'Number of APY',
+        name10: 'Number of Insurance',
+        name11: 'Value of transactions facilitated',
+        name12: 'Number of BC agents',
+        dashName: 'Aggregate'
+    }
+    
+    data.card2 = await countName('DF')
+                + await countName('EFP') 
+                + await countName('GGFPO') 
+                + await countName('SCFP') 
+                + await countName('VC') 
+                + await countName('SCF') 
+    data.card3 = await sumLoan('DFFarmers')
+        + await sumLoan1('EF')
+        + await sumLoan('GGCF')
+        + await sumLoan1('SCFP')
+        + await sumLoan1('VC')
+        + await sumLoan('SCF')
+        
+    data.card4 = await countName('SCV')
+    data.card5 = await sumLoan1('SCV')
+    data.card10 = await countName('VI') 
+                 + await countName('EI') 
+                 + await countName('FI') 
+        + await countName('DInsurance') 
+    
+    data.card9 = await countName('FA') 
+                + await countName('EA') 
+    data.card6 = await countName('SCF')
+                + await countName('FC')
+                + await countName('MC')
+                + await countName('GGCI')
+                + await countName('DFFarmers')
+                + await countName('EFP')
+                + await countName('RC')
+    
+    data.card7 = await sumLoan('SCF')
+                + await sumLoan1('FC')
+                + await sumLoan('MC')
+                + await sumLoan('GGCI')
+                + await sumLoan('DFFarmers')
+                + await sumLoan1('EFP')
+                + await sumLoan1('RC')
+    data.card1 = data.card6
+                 + data.card8
+                    + data.card9
+                    + data.card10
+       
+    data.card8 = await countName('MB')
+        + await countName('FS') 
+    data.card9 = await countName('FA') + await countName('EA')
+    data.card11 = await transactionData('FT')
+        + await transactionData('MT')
+    
+        try {
+
+            const fields = [
+    
+            
+                { label: 'Total Number of Beneficiaries', value: 'card1' },
+                { label: 'Number of FPOs receiving credit', value: 'card2' },
+                { label: 'Credit to FPOs', value: 'card3' },
+                { label: 'Number of VLEs receiving credit', value: 'card4' },  
+                { label:'Value of VLEs credit', value: 'card5' },
+                { label:'Number of Individual farmers receiving Credit', value: 'card6' },
+                { label:'Value of credit to individual farmers', value: 'card7' },
+                { label: 'Number of savings account opened', value: 'card8' },
+                { label: 'Number of APY', value: 'card9' },
+                { label: 'Number of Insurance', value: 'card10' },
+                { label: 'Value of transactions facilitated', value: 'card11' },
+                { label: 'Number of BC agents', value: 'card12' }
+              
+        
+            ]
+           
+            const csv = json2csv(data, {fields});
+            // req.flash('message', 'Download Success')
+            res.attachment(`${data.dashName}.csv`).send(csv)
+            // res.redirect('/ci')
+        } catch (error) {
+            console.log(error)
+            res.status(500).send()
+        }
+})
+
 router.get('/aggregate1', async (req, res) => {
     var data = {
         card1: 0,
--- src/routers/aggregateRouter.js
+++ src/routers/aggregateRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const json2csv = require('json2csv').parse
 
 
 const countName = async function (varr) {
@@ -42,6 +42,87 @@ const sumLoan1 = async function (varr) {
     });
     return num
 }
+
+router.get('/aggregate/data', async (req, res) => {
+    var data = {
+        card1: 0,
+        card2: 0,
+        card3: 0,
+        card4: 0,
+        card5: 0,
+        card6: 0,
+        card7: 0,
+        name1:'Total Beneficiaries',
+        name2:'Savings Account Opened',
+        name3:'Credit Disbursed',
+        name4:'Insurance Policy Issued',
+        name5:'Social Security Schemes',
+        name6:'Digitization (Trnx in Lacs)',
+        name7: 'Banking Outlets/ BCs Opened',
+        dashName: 'Aggregate'
+    }
+    
+    data.card2 = await countName('MB') + await countName('FS') 
+    data.card3 = await sumLoan('DFFarmers')
+        + await sumLoan1('EF')
+        + await sumLoan('GGCF')
+        + await sumLoan1('SCFP')
+        + await sumLoan1('VC')
+        + await sumLoan1('SCV')
+        + await sumLoan('SCF')
+        + await sumLoan1('FC')
+        + await sumLoan('MC')
+        + await sumLoan('GGCI')
+    
+        + await sumLoan('DFFarmers')
+        + await sumLoan1('EFP')
+        + await sumLoan1('RC')
+    data.card4 = await countName('VI') 
+                 + await countName('EI') 
+                 + await countName('FI') 
+        + await countName('DInsurance') 
+    data.card5 = await countName('FA') 
+                + await countName('EA') 
+    data.card6 = await transactionData('FT')
+        + await transactionData('MT')
+    data.card1 = data.card2
+                 + data.card4
+        + data.card5
+        + await countName('SCF')
+        + await countName('FC')
+        + await countName('MC')
+        + await countName('GGCF')
+        + await countName('DFFarmers')
+        + await countName('RC')
+        + await countName('EG')
+    
+        try {
+            const fields = [
+            
+                
+
+               
+                { label: 'Total Beneficiaries', value: 'card1' },
+                { label: 'Savings Account Opened', value: 'card2' },
+                { label: 'Credit Disbursed', value: 'card3' },
+                { label: 'Insurance Policy Issued', value: 'card4' },
+                { label: 'Social Security Schemes', value: 'card5' },
+                { label: 'Digitization (Trnx in Lacs)', value: 'card6' },
+                { label: 'Banking Outlets/ BCs Opened', value: 'card7' },
+                
+        
+            ]
+           
+            const csv = json2csv(data, {fields});
+            // req.flash('message', 'Download Success')
+            res.attachment(`${data.dashName}.csv`).send(csv)
+            // res.redirect('/ci')
+        } catch (error) {
+            console.log(error)
+            res.status(500).send()
+        }
+ })
+
 router.get('/aggregate', async (req, res) => {
     var data = {
         card1: 0,
