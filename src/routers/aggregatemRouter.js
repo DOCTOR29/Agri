056f0a6b2a8ec9e007ff0bcafc96c0542aba2bae
Doctor <naveedislam545@gmail.com>
2024-02-15 10:32:31 +0530

    link

--- src/routers/aggregatemRouter.js
+++ src/routers/aggregatemRouter.js
@@ -62,10 +62,61 @@ router.get('/aggregatem', async (req, res) => {
         name8: 'Female Beneficiaries',
         dashName: 'Aggregate'
     }
-    
+  
     
     
     res.render('aggregateDash', {data})
+})
+router.get('/aggregatem/data', async (req, res) => { 
+    var data = {
+        card1: 30032,
+             card2: 11620,
+             card3: 89814000,
+             card4: 9900,
+             card5: 573,
+             card6: 450581000,
+             card7: 202,
+             card8:15764,
+             name1:'Total Beneficiaries',
+             name2:'Savings Account Opened',
+             name3:'Credit Disbursed',
+             name4:'Insurance Policy Issued',
+             name5:'Social Security Schemes',
+             name6:'Digitization (Trnx in Lacs)',
+             name7: 'Banking Outlets/ BCs Opened',
+             name8: 'Female Beneficiaries',
+             dashName: 'Aggregate'
+    }
+    try {
+
+        const fields = [
+
+        
+            { label: 'Total Number of Beneficiaries', value: 'card1' },
+            { label: 'Number of FPOs receiving credit', value: 'card2' },
+            { label: 'Credit to FPOs', value: 'card3' },
+            { label: 'Number of VLEs receiving credit', value: 'card4' },  
+            { label:'Value of VLEs credit', value: 'card5' },
+            { label:'Number of Individual farmers receiving Credit', value: 'card6' },
+            { label:'Value of credit to individual farmers', value: 'card7' },
+            { label: 'Number of savings account opened', value: 'card8' },
+            { label: 'Number of APY', value: 'card9' },
+            { label: 'Number of Insurance', value: 'card10' },
+            { label: 'Value of transactions facilitated', value: 'card11' },
+            { label: 'Number of BC agents', value: 'card12' }
+          
+    
+        ]
+       
+        const csv = json2csv(data, {fields});
+        // req.flash('message', 'Download Success')
+        res.attachment(`${data.dashName}.csv`).send(csv)
+        // res.redirect('/ci')
+    } catch (error) {
+        console.log(error)
+        res.status(500).send()
+    }
+           
 })
 
 
