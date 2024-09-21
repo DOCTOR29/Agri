11759e65fb25b3e932b82c7cb4a099dbfb5edafa
Doctor <86110344+DOCTOR29@users.noreply.github.com>
2024-09-21 16:48:18 +0530

    Update modelSDiF.js

--- src/models/modelSDiF.js
+++ src/models/modelSDiF.js
@@ -3,31 +3,30 @@ const mongoose = require('mongoose');
 const onboardingSchema = new mongoose.Schema({
   SLNo: {
     type: Number,
-    required: true
-  },
+      },
   DateOfOnboarding: {
     type: Date,
-    required: true
+
   },
   APARTDistrict: {
     type: String,
-    required: true
+
   },
   APARTBlock: {
     type: String,
-    required: true
+   
   },
   Village: {
     type: String,
-    required: true
+    
   },
   Name: {
     type: String,
-    required: true
+    
   },
   Phone: {
     type: Number,
-    required: true
+    
   }
 });
 
